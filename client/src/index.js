b351b7e1c4d5b4b7393e8a8aef1158986524d9bf
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-08-27 17:13:28 -0500

    setup routes and pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+
+import './main.css'
+
+injectTapEventPlugin()
+
+ReactDOM.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <BrowserRouter>
+    	<App />
+    </BrowserRouter>
+  </MuiThemeProvider>),
+document.getElementById('root'))
+
+
